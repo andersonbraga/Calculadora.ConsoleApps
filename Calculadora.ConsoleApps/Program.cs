05d41a47781d9e090902567501f402704267e200
Anderson <anderson.braga@uniplaclages.edu.br>
2023-06-07 13:56:26 -0300

    Add Menu

--- Calculadora.ConsoleApps/Program.cs
+++ Calculadora.ConsoleApps/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
-        }
+            string opcao;
+
+            List<String> operacoes = new List<string>();
+
+            while (true)
+            {
+              
+
+                Console.Clear();
+
+                Console.WriteLine("Calculadora Tabajara 1.0\n");
+
+                Console.WriteLine("Tela Principal\n");
+
+
+                Console.Write("Opção: ");
+                opcao = Console.ReadLine();
+
+
+            }
     }
 }
